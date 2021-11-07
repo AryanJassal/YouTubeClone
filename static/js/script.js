02acda8434795d1ab7bc05b72097e50ecc718009
Aryan Jassal <aryanjassal.au@gmail.com>
2021-11-07 20:19:35 +1100

    initial commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', function()
 {
+    var scrollMultiplier = 200;
+    var scrollTime = 300;
+
     $('#right-arrow').click(function() {
         var scrollPos = $('.categories').scrollLeft();
-        $('categories').animate({scrollLeft: scrollPos + 200 }, 300);
+        $('categories').animate({scrollLeft: scrollPos + scrollMultiplier }, scrollTime);
+        console.log(scrollPos + 200);
+    });
+
+    $('#left-arrow').click(function() {
+        var scrollPos = $('.categories').scrollLeft();
+        $('categories').animate({scrollLeft: scrollPos - scrollMultiplier }, scrollTime);
         console.log(scrollPos + 200);
     });
 });
+
+toggleSideNav = function()
+{
+    if ($('.sidenav').hasClass('sidenav-collapsed')) 
+        $('.sidenav').removeClass('sidenav-collapsed')
+    else
+        $('.sidenav').addClass('sidenav-collapsed')
+}
